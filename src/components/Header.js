0976725304e8bfb841cc299c6aa4730ddac1f215
reasley <reasley.com@gmail.com>
2022-02-08 08:11:34 +0900

    fix: Edit the component
    Edit header component
    - add mobile version style (response)
    
    Edit typography component
    - add small mobile version style (response)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,25 @@ import styles from "./Header.module.css";
 
 function Header() {
     const headerContentList = ['home', 'blog', 'project', 'service']
-    const headerImageList   = { themeLogo: '/theme-logo.png', themeLight: '/theme-light.svg', themeDark: '/theme-dark.svg', themeSearch: '/theme-search.svg' }
+    const headerImageList   = { themeLogo: '/theme-logo.png', themeBar: '/theme-bar.svg', themeLight: '/theme-light.svg', themeDark: '/theme-dark.svg', themeSearch: '/theme-search.svg' }
     return ( 
         <header>
             <div>
                 <img alt='logo' src={ headerImageList.themeLogo } />
-                { headerContentList.map((content) => <div className={ styles.themeMenu }><Link to={ '/' + content }><img alt='menu' src={ '/header-'+content+'.png' } /></Link></div> ) }
+                { headerContentList.map((content) => <div key={ content } className={ styles.themeMenuDesktop }><Link to={ '/' + content }><img alt='menu' src={ '/header-'+content+'.png' } /></Link></div> ) }
             </div>
 
             <div>
-                <div className={ styles.themeMenu }>
+                <div className={ styles.themeMenuDesktop }>
                     <img alt='theme' src={ headerImageList.themeLight } className={ styles.themeLight } />
                 </div>
 
-                <div className={ styles.themeMenu }>
+                <div className={ styles.themeMenuDesktop }>
                     <img alt='search' src={ headerImageList.themeSearch } className={ styles.themeSearch } />
+                </div>
+
+                <div className={ styles.themeMenuMobile }>
+                    <img alt='bar' src={ headerImageList.themeBar } className={ styles.themeSearch } />
                 </div>
             </div>
         </header>
--- src/components/Platform.js
+++ src/components/Platform.js
@@ -2,19 +2,9 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Platform() {
-    const headerContentList = ['home', 'blog', 'project', 'service']
-    const headerImageList   = { themeLogo: '/theme-logo.png', themeLight: '/theme-light.svg', themeDark: '/theme-dark.svg', themeSearch: '/theme-search.svg' }
     return ( 
         <div>
-            <div>
-                <div className={ styles.themeMenu }>
-                    <img alt='theme' src={ headerImageList.themeLight } className={ styles.themeLight } />
-                </div>
-
-                <div className={ styles.themeMenu }>
-                    <img alt='search' src={ headerImageList.themeSearch } className={ styles.themeSearch } />
-                </div>
-            </div>
+            
         </div>
     )
 }
