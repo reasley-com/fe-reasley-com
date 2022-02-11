4b633aed7dc8c54fbf244f5069d08d63311fc079
reasley <reasley.com@gmail.com>
2022-02-12 00:51:47 +0900

    feat: Add new component
    Add notice component

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -3,11 +3,24 @@ import MainTitle from "./MainTitle";
 import styles from "./Notice.module.css";
 
 function Notice() {
+    const noticeContent = [
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+        { title: '[모집] 웹 RPG 게임 프로젝트', category: '개발 공지' },
+    ]
     return ( 
-        <div>
-            <div>
-                <MainTitle title='공 지' />
-            </div>
+        <div className={ styles.notice__frame }>
+            <MainTitle title='공 지' />
+            { noticeContent.map(( content ) => (
+                <div className={ styles.notice__content__frame }>
+                    <span>{ content.title }</span>
+                    <span>{ content.category }</span>
+                </div>
+            )) }
         </div>
     )
 }
