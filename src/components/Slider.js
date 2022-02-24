61d8d3fbdcc28bec7def314c4ebd85c17f491244
reasley <reasley.com@gmail.com>
2022-02-24 21:26:33 +0900

    feat: Add to slider link
    Add function list
    - slider compontnet link

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types"
+import { Link } from "react-router-dom";
 import styles from "./Slider.module.css";
 
 function Slider({ src }) {
     return ( 
         <div className={ styles.slider__frame }>
-            <img src={ src } className={ styles.slider__content } />
+            <Link to={ '/post/1' }>
+                <img src={ src } className={ styles.slider__content } />
+            </Link>
         </div>
     )
 }
