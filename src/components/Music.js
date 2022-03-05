a75629055c4a2184cbe1cb4e2a971db026d573eb
reasley <reasley.com@gmail.com>
2022-03-05 22:41:21 +0900

    feat: Add music function
    Added item
    - edit music volume controler style

--- src/components/Music.js
+++ src/components/Music.js
@@ -9,21 +9,23 @@ function Music() {
     }, [audioStatus])
 
     const vols = () => {
-        audioController({ status: 'volume', volume: document.querySelector('.audio_volume').value })
+        audioController({ status: 'volume', volume: document.querySelector('.music__input__volume').value })
     }
 
     const next = () => {
-        audioController({ status: 'next', volume: document.querySelector('.audio_volume').value })
+        audioController({ status: 'next', volume: document.querySelector('.music__input__volume').value })
     }
 
     return (
         <div className={ styles.music__frame }>
-            <span className={ styles.music__name }>『 {audioStatus.nowPlay.name} 』</span>
+            <div className={ styles.music__name }>『 <marquee scrolldelay={500}>{audioStatus.nowPlay.name}</marquee> 』</div>
             <div className={ styles.music__time }></div>
-            <img className={ styles.music__action } onClick={onClickPlayMusicButton} src={audioStatus.nowStatus === 'play' ? '/music-stop.svg' : '/music-play.svg'} />
-            <img className={ styles.music__action } onClick={next} src='/music-next.svg' />
-            <img className={ styles.music__icon } src='/music-volume.svg' />
-            <input className={'audio_volume'} type='range' orient="vertical" min={0} max={1} step={'0.05'} value={audioStatus.nowVolume} onChange={vols}></input>
+            <img className={ styles.music__icon } onClick={onClickPlayMusicButton} src={audioStatus.nowStatus === 'play' ? '/music-stop.svg' : '/music-play.svg'} />
+            <img className={ styles.music__icon } onClick={next} src='/music-next.svg' />
+            <div className={ styles.music_area }>
+                <img className={ styles.music__icon } src='/music-volume.svg' />
+            </div>
+            <input className={ `${styles.music__input__volume} music__input__volume` } type='range' orient='vertical' min={0} max={1} step={'0.05'} value={audioStatus.nowVolume} onChange={vols}></input>
         </div>
     )
 }
